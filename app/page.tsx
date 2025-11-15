f7033141c8fd4267e34628937d6578236b15f46f
HDUY102 <leduy020702@gmail.com>
2025-11-15 15:37:49 +0700

    Pass store for component child

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@
 import { Provider } from "react-redux";
 import Counter from "./components/Counter";
 import store from "@/store/store";
+import { thunkStore } from "@/store/thunkStore";
+import { sagaStore } from "@/store/sagaStore";
+import { observableStore } from "@/store/observableStore";
 
 export default function Home() {
   return (
     <div>
-      <Provider store={store}><Counter/></Provider>
+      <Provider store={observableStore}><Counter/></Provider>
     </div>
   );
 }
